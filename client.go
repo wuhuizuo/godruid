f506f1231c67a6a8f77ea6860d98a96fa1ebc673
acdn-mpreston <33030320+acdn-mpreston@users.noreply.github.com>
2018-02-25 16:59:27 -0500

    fix - correcting an issue that would leave an open connection when druid query failed.

--- client.go
+++ client.go
@@ -65,13 +65,13 @@ func (c *Client) QueryRaw(req []byte, authToken string) (result []byte, err erro
 	}
 
 	resp, err := c.HttpClient.Do(request)
+	defer func() {
+		resp.Body.Close()
+	}()
 
 	if err != nil {
 		return
 	}
-	defer func() {
-		resp.Body.Close()
-	}()
 
 	result, err = ioutil.ReadAll(resp.Body)
 	if err != nil {
